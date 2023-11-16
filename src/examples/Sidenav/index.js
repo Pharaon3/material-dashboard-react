5a0d58b96bb6f9075ca360367850b1ca8d2290ca
pharaon3 <pharaon313@outlook.com>
2023-11-16 10:50:38 +0900

    Title changed

--- src/examples/Sidenav/index.js
+++ src/examples/Sidenav/index.js
@@ -189,7 +189,7 @@ function Sidenav({ color, brand, brandName, routes, ...rest }) {
           color={sidenavColor}
           fullWidth
         >
-          upgrade to pro
+          Grade Now
         </MDButton>
       </MDBox>
     </SidenavRoot>
