a33a95fd8ccb928030a27a5cb291c289f8ffa917
pharaon3 <pharaon313@outlook.com>
2023-11-20 17:17:13 +0900

    json file

--- src/layouts/tables/data/pendingWagersTableData.js
+++ src/layouts/tables/data/pendingWagersTableData.js
@@ -21,26 +21,30 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 export default function data() {
   const [rows, setRows] = useState([]);
+  let wagerData = [];
+  const [feed, setFeed] = useState([]);
+  const [soccerEvent, setSoccerEvent] = useState(null);
+  const [soccerDetail, setSoccerDetail] = useState(null);
   const getWagerData = () => {
     // const axios = require("axios");
 
-    let config1 = {
-      method: "get",
-      maxBodyLength: Infinity,
-      url: "http://socket.betdata.pro:9595/events/50030074488/",
-      headers: {
-        "HTTP-X-API-KEY": "RwaU4mQfGuh67hEyzSe4I2fdcWSA1i02",
-      },
-    };
+    // let config1 = {
+    //   method: "get",
+    //   maxBodyLength: Infinity,
+    //   url: "http://socket.betdata.pro:9595/events/50030074488/",
+    //   headers: {
+    //     "HTTP-X-API-KEY": "RwaU4mQfGuh67hEyzSe4I2fdcWSA1i02",
+    //   },
+    // };
 
-    axios
-      .request(config1)
-      .then((response) => {
-        console.log(JSON.stringify(response.data));
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    // axios
+    //   .request(config1)
+    //   .then((response) => {
+    //     console.log(JSON.stringify(response.data));
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
 
     let config = {
       method: "get",
@@ -70,6 +74,19 @@ export default function data() {
             eventTimer,
             feedData,
           ] = row.split("|");
+          wagerData.push({
+            wagerID: wagerID,
+            sport: sport,
+            league: league,
+            eventName: eventName,
+            market: market,
+            selectedOption: selectedOption,
+            betPlaced: betPlaced,
+            scorewhen: scorewhen,
+            halfWhen: halfWhen,
+            eventTimer: eventTimer,
+            feedData: feedData,
+          });
           return {
             wagerID: (
               <MDTypography component="div" variant="caption" color="text" fontWeight="medium">
@@ -134,8 +151,107 @@ export default function data() {
         console.log(error);
       });
   };
+  const getResult = () => {
+    wagerData.map((row) => {
+      let feedData = "" + row.feedData;
+      const [feedId, feedEvent, feedSelection] = feedData.split("-");
+      const eventData = getEventData(feedId);
+      switch (row.sport) {
+        case "Soccer":
+          console.log("It's soccer game bet. Id is ", row.wagerID);
+          switch (feedEvent) {
+            case "1": // Moneyline - Who's winner in this game?
+              switch (feedSelection) {
+                case "1": // Bet Home team.
+                  console.log("Bet Home");
+                  if (eventData?.d?.match?.status?.name == "Ended") {
+                    if (eventData?.d?.match?.result?.winner == "home") {
+                      console.log("WIN BET!!!!!!!!!!!!!!!!!!!!!");
+                    } else {
+                      console.log("LOSE BET!!!!!!!!!!!!!!!!!!!!");
+                    }
+                  } else {
+                    console.log("Cannot grade now.");
+                  }
+                  break;
+                case "2": // Bet Draw.
+                  console.log("Bet Draw");
+                  if (eventData?.d?.match?.status?.name == "Ended") {
+                    if (eventData?.d?.match?.result?.winner == "draw") {
+                      console.log("WIN BET!!!!!!!!!!!!!!!!!!!!!");
+                    } else {
+                      console.log("LOSE BET!!!!!!!!!!!!!!!!!!!!");
+                    }
+                  } else {
+                    console.log("Cannot grade now.");
+                  }
+                  break;
+                case "3": // Bet Away.
+                  console.log("Bet Away.");
+                  if (eventData?.d?.match?.status?.name == "Ended") {
+                    if (eventData?.d?.match?.result?.winner == "away") {
+                      console.log("WIN BET!!!!!!!!!!!!!!!!!!!!!");
+                    } else {
+                      console.log("LOSE BET!!!!!!!!!!!!!!!!!!!!");
+                    }
+                  } else {
+                    console.log("Cannot grade now.");
+                  }
+                  break;
+                default:
+                  console.log("Error: ", feedData);
+                  break;
+              }
+              break;
+            default:
+              console.log("feedEvent: ", feedEvent);
+              break;
+          }
+          break;
+        default:
+          break;
+      }
+    });
+  };
+  const getEventData = (feedId) => {
+    return soccerDetail;
+  };
   useEffect(() => {
     getWagerData();
+    const fetchSoccerData = async () => {
+      try {
+        // Fetch data from the JSON file
+        const response = await fetch("./footballEventData.json"); // Update the path accordingly
+        const data = await response.json();
+
+        // Set the retrieved data in the state
+        setSoccerEvent(data);
+      } catch (error) {
+        console.error("Error fetching JSON data:", error);
+      }
+    };
+    const fetchSoccerDetail = async () => {
+      try {
+        // Fetch data from the JSON file
+        const response = await fetch("./footballDetail.json"); // Update the path accordingly
+        const data = await response.json();
+
+        // Set the retrieved data in the state
+        setSoccerDetail(data);
+      } catch (error) {
+        console.error("Error fetching JSON data:", error);
+      }
+    };
+
+    // Call the fetchData function
+    fetchSoccerData();
+    fetchSoccerDetail();
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      getResult();
+    }, 1000);
   }, []);
 
   return {
